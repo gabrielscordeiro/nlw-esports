ff6a773d2a9750297d66194c71e94aebad04e4ac
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-10-13 17:47:01 -0300

    feat: 🎸 Criadas migrations para criação de Games e Ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,15 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
     return response.json([
         { id: 1, name: 'Anúncio 1'},
         { id: 2, name: 'Anúncio 2'},
@@ -10,6 +18,10 @@ app.get('/ads', (request, response) => {
         { id: 4, name: 'Anúncio 4'},
         { id: 5, name: 'Anúncio 5'},
     ])
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
 });
 
 app.listen(3333);
