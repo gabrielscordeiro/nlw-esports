78ede741506e4ccfab086643d8b2cf0a6812cbef
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-10-13 19:59:02 -0300

    feat: 🎸 Adicionada rota para criação de Ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,15 @@
-import express, { request } from 'express';
-import { PrismaClient } from '@prisma/client'
+import express from 'express';
+import cors from 'cors';
+
+
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from './utils/convert-hours-string-to-minutes';
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -25,8 +33,24 @@ app.get('/games', async (request, response) => {
     return response.json(games);
 })
 
-app.post('/ads', (request, response) => {
-    return response.status(201).json([]);
+app.post('/games/:id/ads', async(request, response) => {
+    const gameId = request.params.id;
+    const body:any = request.body;
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel
+        }
+    })
+
+    return response.status(201).json(ad);
 })
 
 app.get('/games/:id/ads', async (request, response) => {
@@ -59,7 +83,9 @@ app.get('/games/:id/ads', async (request, response) => {
 
         return {
             ...ad,
-            weekDays: ad.weekDays.split(',')
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd),
         }
     }));
     
